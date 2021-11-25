83478bca3e5f2eb334c14a224c7147df6cb8b845
syed <s.muazzam.siddiqhi@gmail.com>
2021-11-25 19:57:13 +0530

    commit push

--- src/components/pages/Resume/Resume.js
+++ src/components/pages/Resume/Resume.js
@@ -15,17 +15,21 @@ function Resume() {
       <Particle />
       <Container>
         <Row style={{ justifyContent: "center", position: "relative" }}>
-          <Button variant="primary" href={pdf} target="_blank">
-            <AiOutlineDownload />
-            &nbsp;Get Résumé
-          </Button>
-           <Button variant="primary" href={calendly} target="_blank">
-            <AiOutlineDownload />
-            &nbsp;My Calendar
-          </Button>
+            <Col md={6} className="resume-left">
+              <Button variant="primary" href={pdf} target="_blank">
+              <AiOutlineDownload />
+              &nbsp;Get Résumé
+            </Button>
+          </Col>
+          <Col md={6} className="resume-right">
+            <Button variant="primary" href={calendly} target="_blank">
+              <AiOutlineDownload />
+              &nbsp;My Calendar
+            </Button>
+           </Col>
         </Row>
          <Row style={{ justifyContent: "center", position: "relative" }}>
-         <Col md={6} className="resume-right">
+         <Col md={9} className="resume-center">
             <h3 className="resume-title">Required details in short</h3>
             <Resumecontent
               title="Total Years of experience"
@@ -51,6 +55,8 @@ function Resume() {
         <Row className="resume">
           <Col md={6} className="resume-left">
             <h3 className="resume-title">Experience</h3>
+              <Col md={6} className="resume-left">
+            <h3 className="resume-title">Experience</h3>
             <Resumecontent
               title="White Clarke acquired by IDS infotech and now renamed as Solifi  "
               date="Jan 2020 - Present"
@@ -61,6 +67,9 @@ function Resume() {
         "Have contributed for the Test Automation of project on selenium. "
           ]}
             />
+             </Col>
+              <Col md={6} className="resume-right">
+            <h3 className="resume-title">Experience</h3>
             <Resumecontent
               title="Arisglobal is the biggest and leading mid tier life sciences Product development company."
               date="2015 – 2019"
@@ -72,6 +81,7 @@ function Resume() {
               
               ]}
             />
+             </Col>
              
           </Col>
           <Col md={6} className="resume-right">
@@ -90,7 +100,7 @@ function Resume() {
           </Col>
         </Row>
          <Row style={{ justifyContent: "center", position: "relative" }}>
-         <Col md={6} className="resume-right">
+         <Col md={6} className="resume-center">
             <h3 className="resume-title">SkillSet</h3>
             <Resumecontent
               title="Main Skills"
