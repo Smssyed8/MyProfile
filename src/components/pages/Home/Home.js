a852b0f4951e7f066d4e16ecb9cc9e573ff4da2d
syed <s.muazzam.siddiqhi@gmail.com>
2021-11-26 15:02:23 +0530

    xx

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -28,6 +28,7 @@ function Home() {
               <h1 className="heading-name">
                 I'M
                 <strong  > &nbsp;Syed Muazzam Siddiqhi</strong>
+
               </h1>
 
               <div style={{ paddingLeft: 50, textAlign: "left" }} className="main-name">
@@ -52,6 +53,47 @@ function Home() {
               
 
             </Col>
+    <Col md={7}  xs={12}  className="home-header">
+<Card className="quote-card-view">
+      <Card.Body>
+        <blockquote className="blockquote mb-0">
+          <p style={{ textAlign: "justify" }}>
+            <ImPointRight /> <span className="purple"> Total Years of experience  </span><br/>
+            6 years of Experience in Software Development following Agile.<br/>
+            Worked in Arisglobal for 4 years, White clarke now called Solifi for 2 years<br/>
+            <ImPointRight /><span className="purple"> Relevant years</span><br/>
+            Java 1.7, 1.8 -> 6 years<br/>
+            Spring Boot and microservices -> 2 years<br/>
+            Database/PlSql/Sql -> 3 years<br/>
+            RestFul Api -> 3 years<br/><br/>
+            ################################<br/>
+            Experienced In backend software application development with Java, Spring boot, Microservices, Oracle SQL, PostgreSQL, Restful Architecture, Birt report, MongoDB, Apache camel, Apache ActiveMQ, Javascript, Docker, Teamcity, DS, Vm Template.
+           <br/><br/>
+           Personal details
+          </p>
+          <ul>
+            <li className="about-activity">
+              <ImPointRight /> Email: s.muazzam.siddiqhi@gmail.com
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Phone: +918951631056
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Availability: I may miss phone call, please drop a mail, you can schedule meeting directly after 3 pm.
+            </li>
+             <li className="about-activity">
+              <ImPointRight /> Resume: Latest attached here, please download </li>
+             <li className="about-activity">
+              <ImPointRight /> Need more details?: Request you to drop a mail </li>
+               <li className="about-activity">
+              <ImPointRight /> I am serving notice period: Last working day is Jan 19th 2022 </li>
+          </ul>
+        </blockquote>
+        </col>
+
+
+
+
           </Row>
           
                 
