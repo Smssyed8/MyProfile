d4fe674d198cb63d83018505de96c432fae24594
syed <s.muazzam.siddiqhi@gmail.com>
2021-11-27 21:42:21 +0530

    d

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -21,7 +21,7 @@ function Home() {
   return (
     <section>
       <Container fluid style={{
-        paddingLeft: 02
+        paddingLeft: 2
       }} className="home-section" id="home">
         <Particle />
         <Container className="home-content">
