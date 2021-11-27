29f8fb873c97ef3ef9068f3d64e5dc6f1186a0e5
syed <s.muazzam.siddiqhi@gmail.com>
2021-11-27 19:04:28 +0530

    dd

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -14,23 +14,23 @@ import Card from "react-bootstrap/Card";
 import { ImPointRight } from "react-icons/im";
 
 function Home() {
- const pdf = 'https://drive.google.com/file/d/1trsOB4sbv5gKRQp-bzmmFYhwvPZfBC0g/view?usp=sharing'
+  const pdf = 'https://drive.google.com/file/d/1trsOB4sbv5gKRQp-bzmmFYhwvPZfBC0g/view?usp=sharing'
   const cal = 'https://calendly.com/s-muazzam-siddiqhi'
-return (
+  return (
     <section>
       <Container fluid className="home-section" id="home">
         <Particle />
-      <Container className="home-content">
-        
-        <Row>
-            <Col md={5} xs={12}  sm={12} style={{ paddingBottom: 20, position: "absolute" }}>
-        <div className="developer-animation" >
-        <Tilt><DeveloperAnimation /></Tilt>
-        </div>
-              
+        <Container className="home-content">
+
+          <Row>
+            <Col md={5} xs={12} sm={12} style={{ paddingBottom: 20, position: "absolute" }}>
+              <div className="developer-animation" >
+                <Tilt><DeveloperAnimation /></Tilt>
+              </div>
+
 
             </Col>
-            <Col  xs={12}  className="home-header1">
+            <Col xs={12} className="home-header1">
 
               <h1 style={{ paddingBottom: 15 }} className="heading">
                 Hi There! <span className="wave">👋🏻</span>
@@ -42,85 +42,90 @@ return (
 
               </h1>
 
-            <div style={{ paddingLeft: 50, textAlign: "left" }} className="main-name">
-              <p> Scroll below and copy the details </p>
+              <div style={{ paddingLeft: 50, textAlign: "left" }} className="main-name">
+                <p> Scroll below and copy the details </p>
                 <Type />
                 <br />
                 <br />
                 <br />
-                <Button variant="primary" href={pdf} target="_blank">
-                  <AiOutlineDownload /> 
-                  &nbsp;Get Résumé
-                </Button>
-                <br />
-                <br />
-               <Button variant="primary" href={cal} target="_blank">
-                  <AiOutlineDownload /> 
-                  &nbsp;My Calendar
-                </Button>
-                </div>
+                <div class="buttons">
 
-<br/>
-               
-             
-             
+                  <div class="action_btn">
+                    <Button variant="primary" class="action_btn " href={pdf} target="_blank">
+                      <AiOutlineDownload />
+                      &nbsp;Get Résumé
+                    </Button>
+                    <br />
+                    <br />
+                    <Button variant="" class="action_btn " href={cal} target="_blank">
+                      <AiOutlineDownload />
+                      &nbsp;My Calendar
+                    </Button>
+                  </div>
+                </div>
+              </div>
+
+              <br />
+
+
+
             </Col>
 
           </Row>
-        <Row>
-         
-          
-           
-    <Col  xs={12}  className="home-header">
-<Card className="quote-card-view">
-      <Card.Body>
-        <blockquote className="blockquote mb-0">
-          <p style={{ textAlign: "justify" }}>
-            <ImPointRight /> <span className="purple"> Total Years of experience  </span><br/>
-            6 years of Experience in Software Development following Agile.<br/>
-            Worked in Arisglobal for 4 years, White clarke now called Solifi for 2 years<br/>
-            <ImPointRight /><span className="purple"> Relevant years</span><br/>
-            Java 1.7, 1.8 -> 6 years<br/>
-            Spring Boot and microservices -> 2 years<br/>
-            Database/PlSql/Sql -> 3 years<br/>
-            RestFul Api -> 3 years<br/><br/>
-            ################################<br/>
-            Experienced In backend software application development with Java, Spring boot, Microservices, Oracle SQL, PostgreSQL, Restful Architecture, Birt report, MongoDB, Apache camel, Apache ActiveMQ, Javascript, Docker, Teamcity, DS, Vm Template.
-           <br/><br/>
-           Personal details
-          </p>
-          <ul>
-            <li className="about-activity">
-              <ImPointRight /> Email: s.muazzam.siddiqhi@gmail.com
-            </li>
-            <li className="about-activity">
-              <ImPointRight /> Phone: +918951631056
-            </li>
-            <li className="about-activity">
-              <ImPointRight /> Availability: I may miss phone call, please drop a mail, you can schedule meeting directly after 3 pm.
-            </li>
-             <li className="about-activity">
-              <ImPointRight /> Resume: Latest attached here, please download </li>
-             <li className="about-activity">
-              <ImPointRight /> Need more details?: Request you to drop a mail </li>
-               <li className="about-activity">
-              <ImPointRight /> I am serving notice period: Last working day is Jan 19th 2022 </li>
-          </ul>
-        </blockquote>
-        </Card.Body>
-        </Card>
-        </Col>
+          <Row>
+
+
+
+            <Col xs={12} className="home-header">
+              <Card className="quote-card-view">
+                <Card.Body>
+                  <blockquote className="blockquote mb-0">
+                    <p style={{ textAlign: "justify" }}>
+                      <ImPointRight /> <span className="purple"> Total Years of experience  </span><br />
+                      6 years of Experience in Software Development following Agile.<br />
+                      Worked in Arisglobal for 4 years, White clarke now called Solifi for 2 years<br />
+                      <ImPointRight /><span className="purple"> Relevant years</span><br />
+            Java 1.7, 1.8 -> 6 years<br />
+            Spring Boot and microservices -> 2 years<br />
+            Database/PlSql/Sql -> 3 years<br />
+            RestFul Api -> 3 years<br /><br />
+                      ################################<br />
+                      Experienced In backend software application development with Java, Spring boot, Microservices, Oracle SQL, PostgreSQL, Restful Architecture, Birt report, MongoDB, Apache camel, Apache ActiveMQ, Javascript, Docker, Teamcity, DS, Vm Template.
+                      <br /><br />
+                      Personal details
+                    </p>
+                    <ul>
+                      <li className="about-activity">
+                        <ImPointRight /> Email: s.muazzam.siddiqhi@gmail.com
+                      </li>
+                      <li className="about-activity">
+                        <ImPointRight /> Phone: +918951631056
+                      </li>
+                      <li className="about-activity">
+                        <ImPointRight /> Availability: I may miss phone call, please drop a mail, you can schedule meeting directly after 3 pm.
+                      </li>
+                      <li className="about-activity">
+                        <ImPointRight /> Resume: Latest attached here, please download </li>
+                      <li className="about-activity">
+                        <ImPointRight /> Need more details?: Request you to drop a mail </li>
+                      <li className="about-activity">
+                        <ImPointRight /> I am serving notice period: Last working day is Jan 19th 2022 </li>
+                    </ul>
+                  </blockquote>
+                </Card.Body>
+              </Card>
+            </Col>
 
 
 
 
           </Row>
-          
-                
+
+
         </Container>
 
       </Container>
-     
+
       {/* <Home2 /> */}
       <About />
       {/* <Projects /> */}
